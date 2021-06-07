82e2c15d5d80729338c318198f10d84bdb31da49
nathan-tw <106306027@g.nccu.edu.tw>
2021-06-07 20:07:03 +0800

    add vendor

---
+++ vendor/github.com/adshao/go-binance/v2/delivery/depth_service.go
@@ -0,0 +1,9 @@
+package delivery
+
+import "github.com/adshao/go-binance/v2/common"
+
+// Ask is a type alias for PriceLevel.
+type Ask = common.PriceLevel
+
+// Bid is a type alias for PriceLevel.
+type Bid = common.PriceLevel
