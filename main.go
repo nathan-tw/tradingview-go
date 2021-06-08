ccce2c57b3a39dc222ad656e9e21439058999691
nathan-tw <106306027@g.nccu.edu.tw>
2021-06-08 13:33:44 +0800

    fix log timezone

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/nathan-tw/tradingview-go/src"
 )
 
 func main() {
-	r := gin.Default()
+	location, _ := time.LoadLocation(os.Getenv("TZ"))
+	r := gin.New()
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("[%v]: %3d | %15s | %-7s %#v\n%s",
+			param.TimeStamp.In(location).Format("2006/01/02 - 15:04:05"),
+			param.StatusCode,
+			param.ClientIP,
+			param.Method,
+			param.Path,
+			param.ErrorMessage,
+		)
+	}))
+	r.Use(gin.Recovery())
 	r.GET("/", src.Ping)
 	r.Use(src.LogResponseBody)
 	r.POST("/futures_webhook", src.HandleFuturesStrategy)
